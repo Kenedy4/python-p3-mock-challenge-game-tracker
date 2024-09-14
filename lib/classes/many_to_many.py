130a3fe70f9ccc20d8deb975535bede4b8b954d6
kenedy4 <kennedymusyoki8@gmail.com>
2024-09-14 10:10:48 +0300

    Added missing methods and instance

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,79 @@
 class Game:
     def __init__(self, title):
+        if not isinstance(title, str) or len(title) == 0:
+            raise Exception("Title must be a non-empty string.")
         self.title = title
+    
+    @property
+    def title(self):
+        return self.title
 
     def results(self):
-        pass
+        return [result for result in Result.all() if result.game == self]
 
     def players(self):
-        pass
+        return list(set([result.player for result in self.results()]))
 
     def average_score(self, player):
-        pass
+        scores = [result.score for result in self.results() if result.player == player]
+        return sum(scores) / len(scores) if scores else 0
 
 class Player:
     def __init__(self, username):
+        if not isinstance(username, str) or len(username) == 0:
+            raise Exception("Username must be a non-empty string.")
         self.username = username
+        
+    @property
+    def username(self):
+        return self.username
+    
 
     def results(self):
-        pass
+        return [result for result in Result.all() if result.player == self]
 
     def games_played(self):
-        pass
+        return list(set([result.game for result in self.results()]))
 
     def played_game(self, game):
-        pass
+        return game in self.games_played()
 
     def num_times_played(self, game):
-        pass
+        return len([result for result in self.results()if result.game == game])
+    @classmethod
+    def highest_scored(cls, game):
+        players = game.players()
+        if not players:
+            return None
+        return max(players, key=lambda player: game.average_score(player))
 
 class Result:
+    _all_results = []
     def __init__(self, player, game, score):
+        if not isinstance(player, Player):
+            raise Exception("Player must be a valid Player instance")
+        
+        if not isinstance(game, Game):
+            raise Exception("Game must be a valid Game instance")
+    
+        if not isinstance(score, int) or not (1 <= score <= 5000):
+            raise Exception("Score must be an integer between 1 and 5000")
         self.player = player
         self.game = game
         self.score = score
+        
+    @property
+    def player(self):
+        return self._player
+
+    @property
+    def game(self):
+        return self._game
+
+    @property
+    def score(self):
+        return self._score
+
+    @classmethod
+    def all(cls):
+        return cls._all_results
